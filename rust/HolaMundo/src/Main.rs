7149c8abcff77d239a4bac1a61c31e1604385b2f
yuan4101 <juanantec@unicauca.edu.co>
2025-04-07 20:23:47 -0500

    Hello world rust

--- rust/HolaMundo/src/Main.rs
+++ rust/HolaMundo/src/Main.rs
@@ -1,15 +1,3 @@
-use std::io;
-
 fn main() {
-    println!("Ingresa un número:");
-
-    let mut entrada = String::new();
-
-    io::stdin()
-        .read_line(&mut entrada)
-        .expect("Error al leer la entrada");
-
-    let numero: i32 = entrada.trim().parse().expect("Por favor ingresa un número válido");
-
-    println!("Ingresaste el número: {}", numero);
+    println!("Hello, world!");
 }
